09a7141da29e530989c0e85d88c2c812033eb7a5
stealthqueen0849 <radhika.ladha2022@vitstudent.ac.in>
2024-07-09 20:08:57 +0530

    "updated login, logout and signup functionality"

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,7 +4,6 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {User} from "../models/user.model.js";
 
 const signup = asyncHandler(async (req, res, next)=>{
-
     /*
      * getting the email/username/password from req.body(user)
      * check if the credentials are filled properly or not
@@ -28,20 +27,24 @@ const signup = asyncHandler(async (req, res, next)=>{
             {username}
         ]
     });
-
     if(user){
         throw new ApiError("User already exists", 401)
     }
+
     const createdUser = await User.create({
         email,
         username,
         password
     })
-
     if(!createdUser){
         throw new ApiError("Something went wrong while creating user", 500)
     }
-    return res.status(201).json(new ApiResponse("User created successfully", 201, createdUser))
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse("User created successfully", 201)
+        )
 
 })
 
@@ -55,22 +58,22 @@ const login = asyncHandler(async (req, res, next)=>{
      * generate access token using jwt.sign()
      * send this in cookies
      */
-    const {email, password} = req.body;
-    if([email, password].some((field) => field?.trim() === "")){
+
+    const {username, password} = req.body;
+    if([username, password].some((field) => field?.trim() === "")){
         throw new ApiError("Please fill all the credentials", 401)
     }
-    const user = await User.findOne({
-        $or: [
-            {email},
-            {username}
-        ]
-    });
 
-    const isPasswordValid = await user.isPasswordCorrect(password, this.password)
-
-    if(!isPasswordValid){
-        throw new ApiError("Password invalid", 400)
+    const user = await User.findOne({username});
+    if(!user){
+        throw new ApiError("User not found", 404)
     }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError("Password invalid", 401)
+    }
+
     const accessToken = await user.generateAccessToken()
     if(!accessToken){
         throw new ApiError("Something went wrong while generating token", 500)
@@ -82,9 +85,11 @@ const login = asyncHandler(async (req, res, next)=>{
     }
     return res
             .status(200)
-            .cookie("accesToken", accessToken, options)
+            .cookie("accessToken", accessToken, options)
             .json(
-                new ApiResponse("Login successful", 200, )
+                new ApiResponse("Login successful", 200, {
+                    accessToken
+                })
             )
 
 })
@@ -94,7 +99,21 @@ const logout = asyncHandler(async (req, res, next) =>{
      * check if the token is there or not 
      * delete the cookies 
     */
-   
+    if(!req.cookies?.accessToken){
+        throw new ApiError("No user to logout", 401)
+    }
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .json(
+        new ApiResponse("User logged out succesfully", 200)
+    )
 })
 
 export {
