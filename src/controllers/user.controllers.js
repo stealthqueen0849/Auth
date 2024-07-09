ea121983432f9b65622e14804138dced0e55ed7c
stealthqueen0849 <radhika.ladha2022@vitstudent.ac.in>
2024-07-09 19:06:00 +0530

    "Updated user.controller.js"

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/ApiError.js"
-import ApiResponse from "../utils/ApiResponse.js"
-import { User } from "../models/user.models.js";
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {User} from "../models/user.model.js";
 
 const signup = asyncHandler(async (req, res, next)=>{
 
@@ -96,3 +96,10 @@ const logout = asyncHandler(async (req, res, next) =>{
     */
    
 })
+
+export {
+    signup,
+    login,
+    logout
+
+}
